cf94023e9480a21bb311bb971d1ae650c0f45194
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-05-16 23:02:26 -0500

    Some functions added

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/HelloWorld.java
@@ -3,7 +3,31 @@ package ru.stqa.pft.sandbox;
 public class HelloWorld {
 
   public static void main(String[] args) {
-     System.out.println("Hello, Daria's amazing big world!");
+
+      hello("world");
+      hello("Daria");
+      hello("Pavel");
+      hello("Vita");
+      hello("Vania");
+
+      double len = 5;
+      double a = 7;
+      double b = 9;
+    System.out.println("Площадь со стороной квадрата " + len + " = " + area(len));
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a,b));
+    }
+    public static void hello(String somebody) {
+
+    System.out.println("Hello, " + somebody + "!");
+
+  }
+
+  public static double area(double f) {
+    return f * f;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
   }
 
 }
