ebe2ae42ab778dedd5457a12fa480add8b6a6a4c
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-05-24 15:49:13 -0500

    HelperBase created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -3,12 +3,13 @@ package ru.stqa.pft.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
   FirefoxDriver wd;
+  private SessionHelper sessionHelper;
+  private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
   public static boolean isElementPresent(FirefoxDriver wd) {
@@ -34,29 +35,23 @@ public class ApplicationManager {
     wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     wd.get("http://localhost:8080/addressbook/");
     groupHelper = new GroupHelper(wd);
-    login("admin", "secret");
+    navigationHelper = new NavigationHelper(wd);
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
 
   }
 
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-  }
 
-  public void gotoGroupPage() {
-    groupHelper.returnToGroupPage();
-  }
 
   public void stop() {
     wd.quit();
   }
 
   public GroupHelper getGroupHelper() {
-    return groupHelper;
+    return navigationHelper.groupHelper;
   }
 
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
+  }
 }
