22fa6a06788df4c8fa690e21db63ecdcd733a514
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-06-01 13:23:09 -0500

    navigation helper optimized

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -2,8 +2,6 @@ package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.pft.addressbook.tests.TestBase;
 
 public class NavigationHelper extends HelperBase {
   public GroupHelper groupHelper;
@@ -14,14 +12,25 @@ public class NavigationHelper extends HelperBase {
   }
 
   public void gotoGroupPage() {
+    if (isElementPresent(By.tagName("h1"))
+            && wd.findElement(By.tagName("h1")).getText().equals("groups")
+            && isElementPresent(By.name("new"))) {
+      return;
+    }
     click(By.linkText("groups"));
   }
 
   public void gotoAddNewPage() {
+    if (isElementPresent(By.name("new_group"))) {
+      return;
+    }
     click(By.linkText("add new"));
   }
 
   public void gotoHomePage() {
+    if (isElementPresent(By.id("maintable"))) {
+      return;
+    }
     click(By.linkText("home"));
   }
 }
