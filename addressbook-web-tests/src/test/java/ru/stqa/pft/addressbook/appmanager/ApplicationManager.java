1b140df126d77776855247228eea16edd6f53bf8
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-06-28 12:03:19 -0500

    DbHelper created

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -22,6 +22,7 @@ public class ApplicationManager {
   public GroupHelper groupHelper;
   public ContactHelper contactHelper;
   public String browser;
+  private DbHelper dbHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -32,6 +33,8 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
+    dbHelper = new DbHelper();
 
     if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
@@ -48,7 +51,6 @@ public class ApplicationManager {
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     contactHelper = new ContactHelper(wd);
-
   }
 
   public void stop() {
@@ -67,4 +69,7 @@ public class ApplicationManager {
     return contactHelper;
   }
 
+  public DbHelper db() {
+    return dbHelper;
+  }
 }
