094f0482a585b6a627551a4d2fd9cf7cac38e97b
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-07-07 21:42:22 -0500

    problems with captcha

---
+++ mantis-tests/out/test/resources/config_inc.php
@@ -0,0 +1,11 @@
+<?php
+$g_hostname               = 'localhost:8080';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+$g_signup_use_captcha     = OFF;
+
+$g_crypto_master_salt     = 'YCBhaaD3WGcQttctvcXMf+EySBTEphK/hLnGCs5oAJw=';
+$g_phpMailer_method       = PHPMAILER_METHOD_SMTP;
+$g_smtp_host              = 'localhost:8080';
