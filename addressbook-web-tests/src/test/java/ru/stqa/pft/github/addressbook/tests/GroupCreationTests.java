b3a2f5a02518d6a1ac5e56bcc02700e6eaba4692
Daraja144 <65374897+Daraja144@users.noreply.github.com>
2020-07-30 21:46:21 -0500

    BadGroupCreation spoiled

--- addressbook-web-tests/src/test/java/ru/stqa/pft/github/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/github/addressbook/tests/GroupCreationTests.java
@@ -74,7 +74,7 @@ public class GroupCreationTests extends TestBase {
     app.group().create(group);
     assertThat(app.group().count(), equalTo(before.size()));
     Groups after = app.group().all();
-    assertThat(after, equalTo(before));
+    assertThat(after, equalTo(before.size() -1));
     verifyGroupListInUI();
   }
 
